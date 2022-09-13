941d0917ce6befd377aba2ff8bce87f5f25abb50
Andrew Siems <siemsdrew@gmail.com>
2022-09-13 12:27:54 -0700

    Add methods to find most frequent hour and most frequent day for registration

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,17 +2,6 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 
-def clean_phone_number(number)
-  num_string = number.to_s.gsub(/[^\d]/, '')
-  if num_string.length == 10
-    num_string
-  elsif num_string.length == 11 && num_string[0] == '1'
-    num_string[1..-1]
-  else
-    "Invalid number!"
-  end
-end
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
@@ -42,6 +31,17 @@ end
   #end
 #end
 
+def clean_phone_number(number)
+  num_string = number.to_s.gsub(/[^\d]/, '')
+  if num_string.length == 10
+    num_string
+  elsif num_string.length == 11 && num_string[0] == '1'
+    num_string[1..-1]
+  else
+    "Invalid number!"
+  end
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -63,4 +63,42 @@ contents.each do |row|
   #form_letter = erb_template.result(binding)
 
   #save_thank_you_letter(id,form_letter)
+end
+
+def most_common_hour
+  contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
+  hours = []
+  contents.each do |row|
+    reg_date = row[:regdate]
+    reg_hour = Time.strptime(reg_date, '%m/%d/%y %k:%M').strftime('%k')
+    hours.push(reg_hour)
+  end
+  common_hour = hours.reduce(Hash.new(0)) do |result, hour|
+    result[hour] += 1
+    result
+  end
+  common_hour.key(common_hour.values.max)
+end
+
+def most_common_day
+  contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
+  days = []
+  contents.each do |row|
+    reg_date = row[:regdate]
+    reg_day = Time.strptime(reg_date, '%m/%d/%y').strftime('%A')
+    days.push(reg_day)
+  end
+  common_day = days.reduce(Hash.new(0)) do |result, day|
+    result[day] += 1
+    result
+  end
+  common_day.key(common_day.values.max)
 end
